eb5f757ec326a3627701d6d8c5c1a01d94509109
Yashwanth Gowda sn <yashwan7896@gmail.com>
2026-07-16 14:39:37 +0530

    edited.
    Refactor merge method to include comments on optimization.

--- mergesortedarray.java
+++ mergesortedarray.java
@@ -2,7 +2,7 @@ public class mergesortedarray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
        for (int i = 0; i < n; i++) {
     nums1[m + i] = nums2[i];
-} // should solve again after three pointer concept , not an optimal sol using bubble sort 
+} // should solve again after three pointer concept , not an optimal soln using bubble sort 
         for (int i = 0; i < nums1.length - 1; i++){
             for (int j = 1; j < nums1.length - i; j++) {
                 if (nums1[j] < nums1[j - 1]) {
