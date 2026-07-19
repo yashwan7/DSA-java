f2fe172c404253716b1c4c9ca2bdcf3cdd03adde
yashwan7896-source <yashwan7896@gmail.com>
2026-07-19 11:35:31 +0530

    added find all duplicates on an array

---
+++ lc442findallduplicatesinarray.java
@@ -0,0 +1,42 @@
+
+
+public class lc442findallduplicatesinarray {
+
+    public List<Integer> findDuplicates(int[] nums) {
+      
+         int i= 0;
+       while (i < nums.length) {
+          
+                int correct = nums[i] - 1; // Calculate where this number actually belongs      
+                // If the target spot doesn't already have this number, swap them
+                if (nums[i] != nums[correct]) { 
+                    swap(nums, i, correct);
+                } 
+           
+            else { 
+                i++;
+            } 
+        } 
+           List<Integer> ans = new ArrayList<>();
+              for (int j = 0 ; j< nums.length ; j ++)
+        {   if ( nums[j]!= j+1)     
+              ans.add(nums[j]);     
+        } return ans ;
+       
+    } 
+    
+          
+ 
+           
+     void swap(int [] nums,int first , int second) 
+    { int temp = nums[first];
+        nums[first]= nums[second];
+        nums[second]= temp ; a
+    }   
+    
+
+
+ 
+    
+
+}
