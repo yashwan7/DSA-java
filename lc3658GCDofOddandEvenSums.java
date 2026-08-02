f280e829ed5b9a3891a764219912ee83ad33f97c
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 17:55:44 +0530

    Added lc3658GCDofOddandEvenSums.java and

---
+++ lc3658GCDofOddandEvenSums.java
@@ -0,0 +1,22 @@
+public class lc3658GCDofOddandEvenSums {
+ //class Solution {
+  //  public int gcdOfOddEvenSums(int n) {
+  //      int oddSum =n*n;
+  //      int evenSum = (n+1)*n;
+  //  
+  //      return gcd(oddSum, evenSum);
+  //  }
+//
+  //  public int gcd(int a, int b) {
+  //      while (b != 0) {
+  //          int temp = b;
+  //          b = a % b;
+  //          a = temp;
+  //      }
+  //      return a;
+  //  }
+//}
+class Solution {
+ public int gcdOfOddEvenSums(int n) {
+   return n ; }} // nxn - nxn + n  = n 
+}
