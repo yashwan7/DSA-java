ebce867c45ed5c7894024dccd95e88bc2da64e7d
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 16:05:44 +0530

    Added solution for maximizing pair strength using GCD in Java.

---
+++ lc4010/MaximizePairStrengthUsingGCD.java
@@ -0,0 +1,32 @@
+package lc4010;
+
+public class MaximizePairStrengthUsingGCD {
+class Solution {
+    public long maxPairStrength(int[] nums) {
+      int n = nums.length ;
+        long ans = 0 ;
+        for(int i =0; i<n ; i++){
+            for (int j =i+1;j<n ;j++){
+                long g = gcd(nums[i],nums[j]);
+                long cur = ((long) nums[i]* nums[j])/(g*g);
+                ans = Math.max(ans, cur);
+                
+            }
+        }
+        return ans ;
+    }
+    private long gcd(long a , long b ){
+        while(b!=0){
+            long t = a%b;
+            a=b;
+            b=t;
+            
+        }
+        return a ;
+    }
+}
+
+}
+git status 
+git add .
+git commit -m "Added solution for maximizing pair strength using GCD in Java."
