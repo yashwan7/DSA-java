bce5b78891f62e8af9bb48472b26d9a7b2a16e97
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 18:25:22 +0530

    Added lc53.java and compared with other snippets

---
+++ lc53.java
@@ -0,0 +1,17 @@
+public class lc53 {
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            //"Kya purana subarray continue karu, ya yahin se naya shuru karu?"
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
+}
