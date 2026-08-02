f0d734988a48df5f3af8d8b9111f386ce7d6b77a
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 18:41:07 +0530

    Added lc1346.java and compared with other snippets

---
+++ lc1346.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class lc1346 {
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(2 * arr[i])) {
+                return true;
+            }
+            if (arr[i] % 2 == 0 && set.contains(arr[i] / 2)) {
+                return true;
+            }
+            set.add(arr[i]);
+        }
+
+        return false;
+    }
+}
+}
