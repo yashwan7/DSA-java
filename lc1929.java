54afc3c844517249f75efcedc7de25c2c3e22f88
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 18:20:48 +0530

    added

--- lc1929.java
+++ lc1929.java
@@ -1,3 +1,23 @@
 public class lc1929 {
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+
+        System.arraycopy(nums, 0, ans, 0, nums.length);
+        System.arraycopy(nums, 0, ans, nums.length, nums.length);
+
+        return ans;
+    }
+}
+//class Solution {
+  //  public int[] getConcatenation(int[] nums) {
+     //   int [] ans = new int[2*nums.length] ;
+    //    for(int i = 0 ; i < nums.length;i++ ){
+      //  ans[i] = nums[i];
+        //ans [i+nums.length] = nums[i ];
+   // }
+    //return ans ;
+    //}
+//}
 
 }
