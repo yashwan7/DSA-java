67ede3fd2675e29e0b85abbe80ed9b709272143d
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 18:42:44 +0530

    Added lc3477.java and compared with other snippets

---
+++ lc3477.java
@@ -0,0 +1,21 @@
+public class lc3477 {
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+ 
+        int count = 0 ;
+       for (int i = 0 ; i < fruits.length ; i++) {
+         boolean placed = false;
+         for (int j = 0 ; j < baskets.length ; j ++){
+            if (fruits[i]<=baskets[j]){
+                baskets[j] = -1;
+                   placed = true;
+                    break; }
+            
+            }
+             if (!placed) {
+                count++;
+        }
+    } return count ;
+    }
+}
+}
