c3eb0bbe6322d1e1900e6af8ad57c09a2ebbcbb8
yashwan7896-source <yashwan7896@gmail.com>
2026-08-02 18:30:39 +0530

    Added lc13.java and compared with other snippets

---
+++ lc13.java
@@ -0,0 +1,30 @@
+public class lc13 {
+class Solution {
+    public int romanToInt(String s) {
+        char[] ch = s.toCharArray();
+            int[] val = new int[128];
+        val['I'] = 1;
+        val['V'] = 5;
+        val['X'] = 10;
+        val['L'] = 50;
+        val['C'] = 100;
+        val['D'] = 500;
+        val['M'] = 1000;
+        int nxt = 0 ;
+        int total = 0 ;
+        for(int i = 0 ; i < s.length() ; i ++){
+           int  cur = val[ch[i]];
+    //  compare left with right and total ko  i ko add   if left is big or esle i ko sub      
+            if (i < s.length() - 1) {
+    nxt = val[ch[i + 1]];
+}
+            if (cur < nxt ) total -= cur ;
+            else total += cur ;
+            
+        }
+        return total ;
+        
+    } 
+}
+
+}
